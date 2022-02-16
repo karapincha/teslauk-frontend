50898c31752b8a45e40f4d69be845b7e1c8483e4
Kasun Peiris <peiris@kasun.io>
2022-02-17 00:23:21 +0530

    Update

--- src/components/molecules/PageHeaderVariant/PageHeaderVariant.tsx
+++ src/components/molecules/PageHeaderVariant/PageHeaderVariant.tsx
@@ -13,16 +13,13 @@ export const PageHeaderVariant: FC<PageHeaderVariantProps> = ({
   heading,
   ...restProps
 }: PageHeaderVariantProps) => {
-  const PageHeaderVariantClasses = CN(`page-header-variant flex gap-[48px]`, className)
+  const PageHeaderVariantClasses = CN(`page-header-variant flex w-full gap-[48px]`, className)
 
   return (
     <div className={PageHeaderVariantClasses} {...restProps}>
-      <h1 className='w-fixed w-[300px] text-h1 font-700 text-N-800 flex-shrink-0'>{heading}</h1>
-      <div className='h-[407px] w-[852px]'>
-        <img
-          src={image}
-          className='w-full rounded-[12px] object-cover object-center h-full'
-        />
+      <h1 className='w-fixed w-[300px] flex-shrink-0 text-h1 font-700 text-N-800'>{heading}</h1>
+      <div className='h-[407px] w-full'>
+        <img src={image} className='h-full w-full rounded-[12px] object-cover object-center' />
       </div>
     </div>
   )
