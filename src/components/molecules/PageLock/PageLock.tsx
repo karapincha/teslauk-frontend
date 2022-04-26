e7537c189aeed7363504949f9521b7cd1fe9fa2a
hasinitharupaba <hasinitharu2@gmail.com>
2022-04-27 03:58:56 +0530

    submit-a-guide-locked

--- src/components/molecules/PageLock/PageLock.tsx
+++ src/components/molecules/PageLock/PageLock.tsx
@@ -16,19 +16,19 @@ export const PageLock: FC<PageLockProps> = ({
   ...restProps
 }: PageLockProps) => {
   const PageLockClasses = CN(
-    `page-lock h-[50vh] flex pb-[56px] items-end bg-gradient-to-t from-[#F8FBFD] to-[rgba(248,251,253,0)] fixed bottom-0 via-[#F8FBFD] left-0 right-0`,
+    `page-lock h-[40vh] md:h-[50vh] flex flex-col md:flex-row pb-[56px] items-end bg-gradient-to-t from-[#F8FBFD] to-[rgba(248,251,253,0)] fixed bottom-0 via-[#F8FBFD] left-0 right-0`,
     className
   )
 
   return (
     <div className={PageLockClasses} {...restProps}>
-      <div className='container flex justify-between'>
+      <div className='container flex gap-[24px] lg:gap-0 flex-col lg:flex-row justify-between'>
         <div className='flex flex-col gap-[12px]'>
-          <h2>{heading}</h2>
+          <h2 className='text-center lg:text-left text-h4 lg:text-h2'>{heading}</h2>
           <p className='mb-0 text-base font-500 text-N-600'>{description}</p>
         </div>
 
-        <div className='flex w-full items-end justify-end'>{cta}</div>
+        <div className='flex w-full justify-center lg:items-end lg:justify-end'>{cta}</div>
       </div>
     </div>
   )
